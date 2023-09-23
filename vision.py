46b70e72cc5423f60da31957c7a30940ea7a7b22
Kade J Rabe <kaderabe04@gmail.com>
2023-09-22 19:38:12 -0500

    cone detection actually only detects cone shapes now, also it works really good

--- vision.py
+++ vision.py
@@ -16,48 +16,60 @@ cap = cv2.VideoCapture(camera_index)
 def detect_cone():
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_yellow = np.array([20, 80, 100])
-    upper_yellow = np.array([60, 255, 255])
-
-    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    middle_x = -1
-    middle_y = -1
-
-    for contour in contours:
-        area = cv2.contourArea(contour)
-
-        if area > 250:
-            M = cv2.moments(contour)
-            if M["m00"] != 0:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-
-                middle_x = cX
-                middle_y = cY
-                
-                print(f"Cone: ({cX}, {cY})")
-
-                cv2.circle(frame, (cX, cY), 7, (0, 0, 255), -1)
-                cv2.putText(frame, "Cone Middle", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
-
+    lower_yellow1 = np.array([0, 100, 100])
+    lower_yellow2 = np.array([40, 200, 200])
+    
+    imgThreshLow = cv2.inRange(hsv, lower_yellow1, lower_yellow2)
+    # cv2.imshow("thresh low", imgThreshLow)
+    
+    kernel = np.ones((5,5),np.uint8)
+    
+    threshed_img_smooth = cv2.erode(imgThreshLow, kernel, iterations = 3)
+    threshed_img_smooth = cv2.dilate(threshed_img_smooth, kernel, iterations = 2)
+    
+    smoothed_img = cv2.dilate(threshed_img_smooth, kernel, iterations = 15)
+    smoothed_img = cv2.erode(smoothed_img, kernel, iterations = 10)
+    # cv2.imshow("mask", smoothed_img)
+    
+    edges_img = cv2.Canny(smoothed_img, 100, 200)
+    contours = cv2.findContours(edges_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow("edge", edges_img)
+    
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 2
+    fontColor = (0, 0, 255)
+    lineType = 2
+    
+    for cnt in contours:
+        boundingRect = cv2.boundingRect(cnt)
+        approx = cv2.approxPolyDP(cnt, 0.09 * cv2.arcLength(cnt, True), True)
+        
+        if len(approx) == 3:
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            bottomLeftCornerOfText = (x, y)
+            cv2.putText(frame,'traffic_cone', 
+                bottomLeftCornerOfText, 
+                font, 
+                fontScale,
+                fontColor,
+                lineType)
+            cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+            
 def detect_apriltag():
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     tags = detector.detect(gray)
 
     for tag in tags:
-        if tag.decision_margin >= 20:
+        if tag.decision_margin >= 25:
             corners = tag.corners.astype(int)
 
             print(f"Tag ID: {tag.tag_id}")
             print(f"Tag Center: {tag.center}")
             print(f"Tag Rotation: {tag.pose_R}")
-            print(f"Tag Translation: {tag.pose_t}\n")
-            print(f"Tag Ambiguity; {tag.decision_margin}")
+            print(f"Tag Translation: {tag.pose_t}")
+            print(f"Tag Ambiguity; {tag.decision_margin}\n")
 
             cv2.polylines(frame, [corners], isClosed=True, color=(0, 255, 0), thickness=2)
             
