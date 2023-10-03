0f7c86b8d51f154167267b5f1d75d965659ab583
Kade <kaderabe04@gmail.com>
2023-10-03 18:48:35 -0500

    not rly sure why it doesnt work but ive almost refactored the cone detection
    adding cube detection after i fix this BS

--- gamePieceDetection.py
+++ gamePieceDetection.py
@@ -3,13 +3,94 @@ import numpy as np
 import cv2
 
 class CameraCone(camera.Camera):
-    def __init__(self, cameraIndex: int = 0, lowerYellow: np.array = np.array[0, 100, 100], upperYellow: np.array = np.array[40, 200, 200], arbituaryValue: int = 0.9):
+    def __init__(self, cameraIndex: int = 0, lowerYellow: np.ndarray = np.array([0, 100, 100]), upperYellow: np.ndarray = np.array([40, 200, 200]), arbituaryValue: float = 0.09):
         self.cameraIndex = cameraIndex
         self.cameraStream = cv2.VideoCapture(cameraIndex)
         self.frame = None
+        self.frameHSV = None
         self.lowerYellow = lowerYellow
         self.upperYellow = upperYellow
+        self.contours = None
+        self.cones = ()
         # i dont rly know why it does what it does but it does what it does.
         self.arbituaryValue = arbituaryValue
+        
+    def printConeData(self):
+        # printing out data to console for debugging
+        for cone in self.cones:
+            position = cone[0]
+            area = cone[1]
+            print(f"Position: ({position[0]}, {position[1]})")
+            print(f"Area: {area}")
 
+    def detectCones(self):
+        # resetting data from previous frame
+        self.cones = ()
+        index = 0
+
+        # looping over each contour
+        for cnt in self.contours:
+            # getting perimeter of object
+            perim = cv2.arcLength(cnt, True)
+            
+            # simplifying the vertices
+            approx = cv2.approxPolyDP(cnt, self.arbituaryValue * perim, True)
+
+            # checking if its a triangle or has 3 vertices
+            if len(approx) == 3:
+                index += 1
+                x, y, w, h = cv2.boundingRect(approx)
+                center = (x + (w / 2), y + (h / 2))
+                area = w * h
+                self.cones[index] = (center, area, cnt, (x, y, w, h))                
+
+    def getContours(self):
+        # detecting only specific colors
+        imgThreshLow = cv2.inRange(self.frameHSV, self.lowerYellow, self.upperYellow)
+
+        # simplifying the frames
+        kernel = np.ones((5,5),np.uint8)
+        threshed_img_smooth = cv2.erode(imgThreshLow, kernel, iterations = 3)
+        threshed_img_smooth = cv2.dilate(threshed_img_smooth, kernel, iterations = 2)
+        smoothed_img = cv2.dilate(threshed_img_smooth, kernel, iterations = 15)
+        smoothed_img = cv2.erode(smoothed_img, kernel, iterations = 10)
+
+        # getting edges
+        edges_img = cv2.Canny(smoothed_img, 100, 200)
+
+        # getting contours
+        self.contours, heirarchy = cv2.findContours(edges_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    def renderCones(self):
+        # stuff for printing stuff to camera stream
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 2
+        fontColor = (0, 0, 255)
+        lineType = 2
     
+        # rendering stuff for each cone detected
+        for cone in self.cones:
+            contour = cone[2]
+            x, y, w, h = cone[3]
+            bottomLeftCornerOfText = (x, y)
+            cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            cv2.putText(self.frame,'traffic_cone', 
+                bottomLeftCornerOfText, 
+                font, 
+                fontScale,
+                fontColor,
+                lineType)
+            cv2.drawContours(self.frame, [contour], -1, (0, 255, 0), 2)
+
+    def render(self):
+        self.renderCones()
+        self.renderCameraStream()
+
+    def getLatestFrame(self):
+        ret, self.frame = self.cameraStream.read()
+        self.frameHSV = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+
+    def update(self):
+        self.getLatestFrame()
+        self.getContours()
+        self.detectCones()
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from apriltagDetection import CameraApriltag2D
+from gamePieceDetection import CameraCone
 from apriltag import DetectorOptions
 from cv2 import waitKey
 
-camera1 = CameraApriltag2D()
+apriltagCam1 = CameraApriltag2D()
+coneCam1 = CameraCone()
 
 while True:
-    camera1.update()
-    camera1.render()
-    camera1.printTagData2D()
+    # apriltagCam1.update()
+    # apriltagCam1.render()
+    # apriltagCam1.printTagData2D()
+    # apriltagCam1.getLatestFrame()
+    # apriltagCam1.renderCameraStream()
+
+    coneCam1.getLatestFrame()
+    coneCam1.renderCameraStream()
+    # coneCam1.update()
+    # coneCam1.render()
+    # coneCam1.printConeData()
 
     if waitKey(1) & 0xFF == ord('q'):
         break
