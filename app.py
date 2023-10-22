a34ab40e8f90b9ca74eea2348bd23efe94dc8ca7
KadeJR <kaderabe04@gmail.com>
2023-10-21 19:04:51 -0500

    all the cameras now run on separate threads

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Response, request
 from camera import Camera
-import camera
+import camera as cam
 from detectors.apriltagDetection import ApriltagDetector3D, ApriltagDetector2D
 from detectors.gamePieceDetection import ConeDetector, CubeDetector
 from detectors.gamePieceDetectionML import GamePieceDetectionML
@@ -14,117 +14,214 @@ from queue import Queue
 
 queue = Queue()
 
-def start():
+def startServer():
     app.run(host='0.0.0.0', debug=False, port=8000)
     
-def runCameras():
-    availableCams = camera.getAvailableCameraIndexes()
-    print(availableCams)
-    # availableCams = [0, 2]
-    cams = []
-    # print(availableCams)
-    for cam in availableCams:
-        cams.append([cam, Camera(cam)])
+def startCameras():
+    availableCams = cam.getAvailableCameraIndexes()
+    threads = []
+    for camIndex in availableCams:
+        thread = threading.Thread(target=runCamera, args=(camIndex, ))
+        threads.append(thread)
     
-    print(cams)        
+    for thread in threads:
+        thread.start()
 
+def runCamera(camIndex):
+    camera = Camera(camIndex)
     apriltag2Detector = ApriltagDetector2D()
     apriltag3Detector = ApriltagDetector3D()
     cubeDetector = CubeDetector()
     coneDetector = ConeDetector()
     objDetector = GamePieceDetectionML()
+    
     while True:
-        # print("yo")
         pageDataJSON = open("pageData.json", "r")
         currentViewedCam = int(json.loads(pageDataJSON.read())["currentCamera"])
         pageDataJSON.close()
         
-        cameraSettingsJSON = open("settings.json", "r")
-        cameraSettings = json.loads(cameraSettingsJSON.read())
-        cameraSettingsJSON.close()
+        settingsJSON = open("settings.json", "r")
+        settings = json.loads(settingsJSON.read())
+        settingsJSON.close()
+        
+        # checking if settings exist for the camera, if not creates a json entry
+        try:
+            cameraSettings = settings["cam" + str(camIndex)]
+        except:
+            defaultSettings = {
+                            "cameraSettings": {
+                               "connectVerbose": "1",
+                               "brightness": "50",
+                               "contrast": "50",
+                               "saturation": "50",
+                               "hue": "50",
+                               "gamma": "120",
+                               "sharpness": "50",
+                               "autoExposure": true
+                            },
+                            "pipelineSettings": {
+                               "toggles": [
+                                  False,
+                                  False,
+                                  False,
+                                  False,
+                                  False
+                               ],
+                               "apriltag2D": {
+                                  "family": "1",
+                                  "nthreads": "2",
+                                  "quadDecimate": 0.2,
+                                  "quadBlur": 0,
+                                  "refineEdges": True,
+                                  "refineDecode": False,
+                                  "refinePose": False,
+                                  "quadContours": False,
+                                  "decisionMargin": "5"
+                               },
+                               "apriltag3D": {
+                                  "family": "1",
+                                  "nthreads": "4",
+                                  "quadDecimate": 0.2,
+                                  "quadBlur": 0.3,
+                                  "refineEdges": True,
+                                  "refineDecode": False,
+                                  "refinePose": False,
+                                  "quadContours": False,
+                                  "decisionMargin": "22",
+                                  "fov": "70"
+                               },
+                               "gamePieceGeo": {
+                                  "lowerPurple": [
+                                     "67",
+                                     "47",
+                                     "23"
+                                  ],
+                                  "upperPurple": [
+                                     "183",
+                                     "250",
+                                     "252"
+                                  ],
+                                  "arbituaryValueCube": "8",
+                                  "lowerYellow": [
+                                     "0",
+                                     "109",
+                                     "100"
+                                  ],
+                                  "upperYellow": [
+                                     "140",
+                                     "146",
+                                     "221"
+                                  ],
+                                  "arbituaryValueCone": "10"
+                               },
+                               "gamePieceML": {},
+                               "retroreflective": {
+                                  "lowerGreen": [
+                                     0,
+                                     0,
+                                     0
+                                  ],
+                                  "upperGreen": [
+                                     0,
+                                     0,
+                                     0
+                                  ],
+                                  "lightOn": True
+                               }
+                            }}
+            settingsJSON = open("settings.json", "w")
+            settings = json.loads(settingsJSON.read())
+            settings["cam" + str(camIndex)] = defaultSettings
+            settingsJSON.write(json.dumps(settings))
+            settingsJSON.close()
+            
+        cameraSettings = settings["cam" + str(camIndex)]
+        try:
+            camera.getCalibrationInfo()
+            apriltag3Detector.setCamParams(camera.params)
+            frame = camera.getLatestFrame()
+            labeledFrame = frame
+            
+            # checking if apriltag2D is enabled
+            if cameraSettings["pipelineSettings"]["toggles"][0]:
+                # gettings and updating the latest options
+                # TODO only update settings when they are actually changed in order to increase performance
+                optionsDict = cameraSettings["pipelineSettings"]["apriltag2D"]
+                options = DetectorOptions(
+                    families="tag16h5",
+                    nthreads=int(optionsDict["nthreads"]),
+                    quad_decimate=optionsDict["quadDecimate"],
+                    quad_blur=optionsDict["quadBlur"],
+                    refine_edges=optionsDict["refineEdges"],
+                    refine_decode=optionsDict["refineDecode"],
+                    refine_pose=optionsDict["refinePose"],
+                    quad_contours=optionsDict["quadContours"]
+                    )
+                apriltag2Detector.updateOptions(options)
+                apriltag2Detector.updateDecisionMargin(float(optionsDict["decisionMargin"]))
                 
-        for cam in cams:
-            try:
-                # print(-1)
-                # print(cam)
-                cam[1].getCalibrationInfo()
-                apriltag3Detector.setCamParams(cam[1].params)
-                frame = cam[1].getLatestFrame()
-                labeledFrame = frame
-                cameraSettingsDict = cameraSettings["cam" + str(cam[0])]
-                # print(0)
-                if cameraSettingsDict["pipelineSettings"]["toggles"][0]:
-                    optionsDict = cameraSettingsDict["pipelineSettings"]["apriltag2D"]
-                    options = DetectorOptions(
-                        families="tag16h5",
-                        nthreads=int(optionsDict["nthreads"]),
-                        quad_decimate=optionsDict["quadDecimate"],
-                        quad_blur=optionsDict["quadBlur"],
-                        refine_edges=optionsDict["refineEdges"],
-                        refine_decode=optionsDict["refineDecode"],
-                        refine_pose=optionsDict["refinePose"],
-                        quad_contours=optionsDict["quadContours"]
-                        )
-                    apriltag2Detector.updateOptions(options)
-                    apriltag2Detector.updateDecisionMargin(float(optionsDict["decisionMargin"]))
-                    labeledFrame = apriltag2Detector.update(labeledFrame, frame)
-                # print(1)
-                if cameraSettingsDict["pipelineSettings"]["toggles"][1]:
-                    optionsDict = cameraSettingsDict["pipelineSettings"]["apriltag3D"]
-                    options = DetectorOptions(
-                        families="tag16h5",
-                        nthreads=int(optionsDict["nthreads"]),
-                        quad_decimate=optionsDict["quadDecimate"],
-                        quad_blur=optionsDict["quadBlur"],
-                        refine_edges=optionsDict["refineEdges"],
-                        refine_decode=optionsDict["refineDecode"],
-                        refine_pose=optionsDict["refinePose"],
-                        quad_contours=optionsDict["quadContours"]
-                        )
-                    apriltag3Detector.updateOptions(options)
-                    apriltag3Detector.updateDecisionMargin(float(optionsDict["decisionMargin"]))
-                    labeledFrame = apriltag3Detector.update(labeledFrame, frame)
-                # print(2)
-                if cameraSettingsDict["pipelineSettings"]["toggles"][2]:
-                    optionsDict = cameraSettingsDict["pipelineSettings"]["gamePieceGeo"]
-                    # print(0)
-                    lowerYellow = np.array([int(optionsDict["lowerYellow"][0]), int(optionsDict["lowerYellow"][1]), int(optionsDict["lowerYellow"][2])])
-                    upperYellow = np.array([int(optionsDict["upperYellow"][0]), int(optionsDict["upperYellow"][1]), int(optionsDict["upperYellow"][2])])
-                    coneDetector.updateYellow(lowerYellow, upperYellow)
-                    coneDetector.updateArbituaryValue(float(optionsDict["arbituaryValueCone"]) / 100)
-                    # labeledFrame = cam["gamePieceGeoCube"].update(labeledFrame, frame)
-                    # print(1)
-                    lowerPurple = np.array([int(optionsDict["lowerPurple"][0]), int(optionsDict["lowerPurple"][1]), int(optionsDict["lowerPurple"][2])])
-                    upperPurple = np.array([int(optionsDict["upperPurple"][0]), int(optionsDict["upperPurple"][1]), int(optionsDict["upperPurple"][2])])
-                    cubeDetector.updatePurple(lowerPurple, upperPurple)
-                    cubeDetector.updateArbituaryValue(float(optionsDict["arbituaryValueCube"]) / 100)
-                    # labeledFrame = cam["gamePieceGeoCube"].update(labeledFrame, frame)
-                    # print(cam["gamePieceGeoCube"])
-                    labeledFrame = cubeDetector.update(labeledFrame, frame)
-                    labeledFrame = coneDetector.update(labeledFrame, frame)
-                    # print(2)
-                # print(3)
-                if cameraSettingsDict["pipelineSettings"]["toggles"][3]:
-                    objDetector.detectInFrame(labeledFrame)
-                # cam["cameraStream"].renderCameraStream(labeledFrame)
-                # print(4)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-                # print(5)
-                # print(cam)
-                if cam[0] == currentViewedCam:
-                    frameBytes = cam[1].convertFrameToBytes(labeledFrame)
-                    cam[1].renderCameraStream(labeledFrame)
-                    # print("bro")
-                    queue.put(frameBytes)
-                    # print(frameBytes)
+                # running the detector
+                labeledFrame = apriltag2Detector.update(labeledFrame, frame)
+            
+            # checking if apriltag3D is enabled
+            if cameraSettings["pipelineSettings"]["toggles"][1]:
+                # gettings and updating the latest options
+                # TODO only update settings when they are actually changed in order to increase performance
+                optionsDict = cameraSettings["pipelineSettings"]["apriltag3D"]
+                options = DetectorOptions(
+                    families="tag16h5",
+                    nthreads=int(optionsDict["nthreads"]),
+                    quad_decimate=optionsDict["quadDecimate"],
+                    quad_blur=optionsDict["quadBlur"],
+                    refine_edges=optionsDict["refineEdges"],
+                    refine_decode=optionsDict["refineDecode"],
+                    refine_pose=optionsDict["refinePose"],
+                    quad_contours=optionsDict["quadContours"]
+                    )
+                apriltag3Detector.updateOptions(options)
+                apriltag3Detector.updateDecisionMargin(float(optionsDict["decisionMargin"]))
+                
+                # running the detector
+                labeledFrame = apriltag3Detector.update(labeledFrame, frame)
+            
+            # checking if Game Piece Detection Geometry is enabled
+            if cameraSettings["pipelineSettings"]["toggles"][2]:
+                # gettings and updating the latest options
+                # TODO only update settings when they are actually changed in order to increase performance
+                optionsDict = cameraSettings["pipelineSettings"]["gamePieceGeo"]
+                lowerYellow = np.array([int(optionsDict["lowerYellow"][0]), int(optionsDict["lowerYellow"][1]), int(optionsDict["lowerYellow"][2])])
+                upperYellow = np.array([int(optionsDict["upperYellow"][0]), int(optionsDict["upperYellow"][1]), int(optionsDict["upperYellow"][2])])
+                coneDetector.updateYellow(lowerYellow, upperYellow)
+                coneDetector.updateArbituaryValue(float(optionsDict["arbituaryValueCone"]) / 100)
+                lowerPurple = np.array([int(optionsDict["lowerPurple"][0]), int(optionsDict["lowerPurple"][1]), int(optionsDict["lowerPurple"][2])])
+                upperPurple = np.array([int(optionsDict["upperPurple"][0]), int(optionsDict["upperPurple"][1]), int(optionsDict["upperPurple"][2])])
+                cubeDetector.updatePurple(lowerPurple, upperPurple)
+                cubeDetector.updateArbituaryValue(float(optionsDict["arbituaryValueCube"]) / 100)
+                
+                # running the detectors
+                labeledFrame = cubeDetector.update(labeledFrame, frame)
+                labeledFrame = coneDetector.update(labeledFrame, frame)
 
-            except:
+    
+            # checking if Game Piece Detection Machine Learning is enabled
+            if cameraSettings["pipelineSettings"]["toggles"][3]:
+                # TODO refactor this class
+                objDetector.detectInFrame(labeledFrame)
+                
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+            
+            print(camIndex, currentViewedCam)
+            if camIndex == currentViewedCam:
+                print(camIndex)
+                frameBytes = camera.convertFrameToBytes(labeledFrame)
+                # camera.renderCameraStream(labeledFrame)
+                queue.put(frameBytes)
 
-thread1 = threading.Thread(target=runCameras)
-
-thread1.start()
+        except:
+            break
+        
+startCameras()
 
 app = Flask(__name__)
 app.config['SERVER_NAME'] = 'chickenvision:8000'
@@ -210,7 +307,7 @@ def settingsTemplate():
     html = open('templates/settings/settings.html', 'r')
     return json.dumps({"data": html.read()})
 
-start()
+startServer()
 
 # if __name__ == '__main__':
 #     threading.Thread(target=start, daemon=True).start()
