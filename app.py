5686373b690784b0316033edf2c51edf2a26c61d
TotallyNotNero <neromisis2015@gmail.com>
2023-10-24 23:37:07 -0500

    better networktables(?)

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import cv2
 import numpy as np
 import threading
 from queue import Queue
-from networktables import NetworkTables
+from networktables import NetworkTables, NetworkTablesInstance
 
 # setup getting general settings
 generalSettings = open("generalSettings.json", "r")
@@ -28,8 +28,10 @@ settingsUpdated = Queue()
 pageDataUpdate = Queue()
 
 # network tables configuration
-NetworkTables.initialize(server='roborio-' + str(teamNumber) + '-frc.local')
-nt = NetworkTables.getTable('ChickenVision')
+ntInst = NetworkTablesInstance.getDefault()
+ntInst.startClientTeam(3082, 5810)
+ntInst.startDSClient()
+nt = ntInst.getTable('ChickenVision')
 
 def startServer():
     app.run(host='0.0.0.0', debug=False, port=8000)
